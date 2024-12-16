665c4d14488d84bc389ef2d21c0cf282d906b385
AnnaRIFFL <113025313+AnnaRIFFL@users.noreply.github.com>
2024-12-16 20:15:53 +0300

    features

---
+++ features.py
@@ -0,0 +1,71 @@
+workclass_data = [
+    'Without-pay',
+    'Local-gov',
+    'Private',
+    'Federal-gov',
+    'Self-emp-inc',
+    'State-gov',
+    'Self-emp-not-inc',
+    'Never-worked'
+]
+
+education_data = [
+    'Preschool',
+    '1st-4th',
+    '5th-6th',
+    '7th-8th',
+    '9th',
+    '10th',
+    '11th',
+    '12th',
+    'HS-grad',
+    'Some-college',
+    'Bachelors',
+    'Assoc-acdm',
+    'Doctorate',
+    'Masters',
+    'Assoc-voc',
+    'Prof-school',
+]
+marital_status_data = [
+    'Never-married',
+    'Divorced',
+    'Married-civ-spouse',
+    'Separated',
+    'Married-spouse-absent',
+    'Widowed',
+    'Married-AF-spouse'
+]
+
+occupation_data = [
+    'Farming-fishing',
+    'Sales',
+    'Prof-specialty',
+    'Tech-support',
+    'Adm-clerical',
+    'Machine-op-inspct',
+    'Protective-serv',
+    'Transport-moving',
+    'Exec-managerial',
+    'Craft-repair',
+    'Handlers-cleaners',
+    'Priv-house-serv',
+    'Armed-Forces',
+    'Other-service',
+]
+relationship_data = [
+    'Not-in-family',
+    'Own-child',
+    'Husband',
+    'Unmarried',
+    'Wife',
+    'Other-relative'
+]
+race_data = [
+    'Black',
+    'White',
+    'Amer-Indian-Eskimo',
+    'Asian-Pac-Islander',
+    'Other'
+]
+sex_data = ['Male', 'Female']
